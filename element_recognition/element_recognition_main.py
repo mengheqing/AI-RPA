ea072c756ddbfb2cd40ebe35c470af8157950766
{mengheqing@mengniu.cn} <mengheqing@mengniu.cn>
2025-06-20 13:48:07 +0800

    add path

--- element_recognition/element_recognition_main.py
+++ element_recognition/element_recognition_main.py
@@ -1,3 +1,8 @@
+import sys
+import os
+curPath = os.path.abspath(os.path.dirname(__file__))
+rootPath = os.path.split(curPath)[0]
+sys.path.append(rootPath)
 from element_recognition.element_recognition_server import app
 import element_recognition.controller
 
